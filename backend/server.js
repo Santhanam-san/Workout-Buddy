5f5ef2ea5d63ea0f545d680dbd5fa2f116da65d6
Ranjith V S <ranararan@gmail.com>
2022-11-29 14:38:19 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 require('dotenv').config()
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config({path: __dirname+'/.env'});
+}
+
 const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
